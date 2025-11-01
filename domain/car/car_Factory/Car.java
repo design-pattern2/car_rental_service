f0e1992f600d1cf693fef7e97793aa3e2bfcc835
soqurqks <kim00566@naver.com>
2025-11-01 16:06:47 +0900

    car_Factory

---
+++ domain/car/car_Factory/Car.java
@@ -0,0 +1,33 @@
+package car_Factory;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Car {
+	private final String id;
+	private final CarType type;
+	private CarStatus status;
+    private BigDecimal dailyRentalFee;
+    
+    
+    public Car(String id, CarType type)
+    {
+    	this.id = Objects.requireNonNull(id);
+        this.type = Objects.requireNonNull(type);
+        this.status = CarStatus.AVAILABLE;
+    }
+    
+    public String id() { return id; }
+    public CarType type() { return type; }
+    public CarStatus status() { return status; }
+    public BigDecimal getDailyRentalFee(){return dailyRentalFee;}
+    
+    public void occupy() { this.status = CarStatus.UNAVAILABLE; }
+    public void release() { this.status = CarStatus.AVAILABLE; }
+    
+    
+    @Override public String toString() {
+    	return "Room{id='%s', type=%s, status=%s}".formatted(id, type, status);
+    }
+    
+}
